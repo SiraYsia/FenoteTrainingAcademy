6ff6705438e300643a753bb4d95108b6e1538783
Goiwaj <zewselam27@gmail.com>
2024-09-16 17:13:08 -0400

    fix date for hsi all ages

--- schedule_folder/js/hsi.js
+++ schedule_folder/js/hsi.js
@@ -16,7 +16,7 @@ const courseDetails = {
     descriptionFile: "schedule_folder/descriptions/hsi/all_ages.html",
     dates: [
       { "start": "2024-09-14", "end": "", "startTime": "10:00", "endTime": "12:15" },
-      { "start": "2024-09-22", "end": "", "startTime": "10:00", "endTime": "12:15" }
+      { "start": "2024-09-22", "end": "", "startTime": "15:00", "endTime": "17:15" }
     ],
   },
   
